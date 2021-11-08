810e12baf909a0e3e8c2fd7cb77b5acbe8412950
Glitch (glitch-hello-website) <none>
2021-11-08 17:42:01 +0000

    👭⏰ Checkpoint ./script.js:38114310/67

--- script.js
+++ script.js
@@ -5,9 +5,9 @@ function getFromServer(){
 
 function mydata(data){
       console.log(data);
-      var text ="<table width class='table table-striped table-dark'><thead><td>User ID</td> <td>Name</td> <td>Email</td></thead><tbody>";
+      var text ="<table width class='table table-striped table-dark'><thead><td>Cust ID</td> <td>Password</td> <td>Name</td><td>Gender</td></thead><tbody>";
       data.forEach(function(item){    
-      text = text + `<tr><td> ${item.custId} </td> <td> ${item.pwd} ${item.name} </td>  
+      text = text + `<tr><td> ${item.custId} </td> <td> ${item.pwd} </td> <td> ${item.name} </td>  
                   <td> ${item.gender} </td> </tr>`
       
       });
