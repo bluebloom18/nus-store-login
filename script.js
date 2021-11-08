e8f0e87f74f8dac378c315d1014ea9b36b06ac79
Glitch (glitch-hello-website) <none>
2021-11-08 18:12:41 +0000

    🐔🐗 Checkpoint ./index.html:38114310/3143 ./script.js:38114310/1652 ./style.css:38114310/4158

--- script.js
+++ script.js
@@ -1,28 +1,25 @@
-function getFromServer(){
-    $.getJSON('https://nusstore.glitch.me/cust', mydata );
-    }
+//syntax: $.getJSON(url, [data],[callback]);
+//AJAX library  fdas,mnfmnasd
+///fdslnf dsfnj
 
-function mydata(data){
-var text = "<p>" `<p class="display-1"> Account: ${item.user_id}, ${item.first_name} ${item.last_name}  
-              <br> Email: ${item.email} </p>`
+const uid = document.getElementById('userid');
+const b1 = document.getElementById('b1');
 
-data.forEach(function(item){    
-      text = text + `<tr><td> ${item.custId} </td> <td> ${item.pwd} </td> <td> ${item.name} </td>  
-                  <td> ${item.gender} </td> </tr>`
-      
-      });
-
-}
-/* TABLE
-function mydata(data){
-      //console.log(data);
-      var text ="<table width class='table table-striped table-dark'><thead><td>Cust ID</td> <td>Password</td> <td>Name</td><td>Gender</td></thead><tbody>";
-      data.forEach(function(item){    
-      text = text + `<tr><td> ${item.custId} </td> <td> ${item.pwd} </td> <td> ${item.name} </td>  
-                  <td> ${item.gender} </td> </tr>`
-      
-      });
-      text += "</tbody></table>"
-      $(".mypanel").html(text);   
+function getFromServer(e){
+  e.preventDefault();       //?
+  var querryStr =  "https://nusstore.glitch.me/cust?user_id=" + uid.value;
+  //console.log (querryStr);
+  $.getJSON(querryStr, mydata );
   }
-*/
+
+function mydata(data){
+  var item = data[0];
+  var text =  `<p class="display-1"> Account: ${item.user_id}, ${item.first_name} ${item.last_name}  
+              <br> Email: ${item.email} </p>`
+  $(".mypanel").html(text);
+}
+b1.addEventListener('click',getFromServer);
+
+
+
+   
