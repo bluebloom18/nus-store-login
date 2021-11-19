4444ac6faee952d4a3e016f9fbf127390bd6d203
Lien Ber Luen <lienbl@gmail.com>
2021-11-19 19:40:44 +0800

    final

--- script.js
+++ script.js
@@ -16,8 +16,11 @@ function getFromServer(e){
 function mydata(data){    
   //console.log(data);
   if (data == true) {
+    document.getElementById("form").innerHTML = "";
+    
     getFromServer1();
     //window.location.href = "welcome.html" 
+
   }
   else {
     var text = "<p>Try Again </p>";
@@ -27,7 +30,7 @@ function mydata(data){
 }
 
 function getFromServer1(){
-    $.getJSON('https://nusstore.glitch.me/cust', mydata1 );
+     $.getJSON('https://nusstore.glitch.me/items', mydata1 );
   }
 
 
@@ -35,12 +38,28 @@ function getFromServer1(){
 function mydata1(data){
   //console.log(data);
   
-  var text = "<p>"; 
+  //var text = "<p>"; 
+
+  var text = `<div class="row px-5 pt-5">`
+
   data.forEach(function(item){    
-      text = text + `CustID: ${item.custId}, Password: ${item.pwd}, Name:  ${item.name}, 
-                  Gender: ${item.gender} <br />`;    //Template Literal
-      
+      //text = text + `itemID: ${item.itemId},  Name:  ${item.name}, Price: ${item.price}, 
+       //           pic: ${item.pic} <br />`;    //Template Literal
+
+       text = text +`    <div class="col-md-4 mt-4 mt-sm-0 card-container">
+       <div class="card text-center product p-4 pt-5 border-0 h-100 rounded-0">
+       <div class="card-body p-4 py-0 h-xs-440p">
+       <h5 class="card-title font-weight-semi-bold mb-3 w-xl-220p mx-auto"> ${item.name}</h5>
+       <p class="price">\$ ${item.price} <p> 
+       </div>
+       <p class="btn w-100 px-4 text-center mx-auto">
+         <input type="submit" class="btn btn-dark btn-lg w-100" name="add-button" value="Buy Now"></p>
+     </div>
+   </div>`;
+
       });
+
+      text = text+ `<div>`;
   
       $(".mypanel").html(text);
 }
